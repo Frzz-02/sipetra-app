a33c5fd312068af25200ec3623ee53404cc18de5
daymouse <setyajilintang@gmail.com>
2025-06-21 16:52:14 +0700

    meambah dashboard user

---
+++ app/Http/Controllers/dashboard_user.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Hewan;
+
+class dashboard_user extends Controller
+{
+    public function index()
+    {
+        return view('dashboard_users');
+    }
+    public function showhewan()
+    {
+        $user = Auth::user(); // user yang login
+
+        $hewan = Hewan::where('id_user', $user->id)->get();
+
+       return view('dashboard_users', compact('hewan'));
+    }
+
+
+}
