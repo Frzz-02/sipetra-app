dde9d965c27ed4b3e8191a9238b5b1bb0c4a6e91
Frzz-02 <pc.feriirawan0211@gmail.com>
2025-06-27 20:19:05 +0700

    menambah component animal card untuk data hewan

---
+++ resources/views/components/animal-card.blade.php
@@ -0,0 +1,23 @@
+<div class="col-md-6 col-12 mb-4">
+    <div class="card border-left-warning shadow h-100">
+        <div class="card-body">
+            <div class="row no-gutters align-items-center d-flex flex-row-reverse">
+              
+                <div class="col-custom pl-2">
+                    <div class="fs-5 font-weight-bold text-warning text-outline text-capitalize mb-0">
+                        Jaran
+                    </div>
+                    <div class="h5 font-weight-light fs-3 mb-0 text-gray-800">rong dolar</div>
+                </div>
+
+                <div class="col">
+                  <div class="d-flex justify-content-center">
+                    <img src="{{ asset('assets/image/catAndBook.png') }}" width="47" height="47" alt="" class="rounded-circle border border-dark border-1">
+                  </div>
+                </div>
+                
+                
+            </div>
+        </div>
+    </div>
+</div>
