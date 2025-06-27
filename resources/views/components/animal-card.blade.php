02bf66cef90681d5c833229ec656e5ff5ac4e8cb
Frzz-02 <pc.feriirawan0211@gmail.com>
2025-06-28 06:47:47 +0700

    membuat animal-card component responsif

--- resources/views/components/animal-card.blade.php
+++ resources/views/components/animal-card.blade.php
@@ -1,18 +1,19 @@
-<div class="col-md-6 col-12 mb-4">
+<div class="col-md-6 col-12 mb-sm-4 mb-2">
     <div class="card border-left-warning shadow h-100">
-        <div class="card-body">
-            <div class="row no-gutters align-items-center d-flex flex-row-reverse">
+        <div class="card-body p-2">
+            <div class="row no-gutters align-items-center d-flex flex-row-reverse pr-1">
               
-                <div class="col-custom pl-2">
-                    <div class="fs-5 font-weight-bold text-warning text-outline text-capitalize mb-0">
-                        Jaran
+                <div class="col-xl-10 col-8 col-sm-10 col-md-9 pl-2">
+                    <div class="font-weight-medium text-warning text-outline text-capitalize mb-0 p-0 text-truncate" style="max-width: 100%">
+                        Jaranddaad
                     </div>
-                    <div class="h5 font-weight-light fs-3 mb-0 text-gray-800">rong dolar</div>
+                    <div class="h5 font-weight-light fs-3 mb-1 text-gray-800">rong dolar</div>
                 </div>
 
                 <div class="col">
                   <div class="d-flex justify-content-center">
-                    <img src="{{ asset('assets/image/catAndBook.png') }}" width="47" height="47" alt="" class="rounded-circle border border-dark border-1">
+                    <img src="https://picsum.photos/40" alt="" class="img-size img-size-max rounded-circle border border-dark border-1">
+                    {{-- width="47" height="47" --}}
                   </div>
                 </div>
                 
